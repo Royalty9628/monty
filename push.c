6d81b2e6cbc4d45dd15e187d6e0d4b095bd42939
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-10-05 09:09:50 -0700

    push integer

--- push.c
+++ push.c
@@ -1,20 +1,21 @@
 #include "monty.h"
+#include <ctype.h>
 
 /**
- * check_for_digit - checks that a string only contain digits
+ * check_for_digit - checks that a string only contains digits
  * @arg: string to check
- * Return: 0 if only digit, else 1
+ * Return: 0 if only digits, else 1
  */
 
 static int check_for_digit(char *arg)
 {
-	int A;
+	int i;
 
-	for (A = 0; arg[A]; A++)
+	for (i = 0; arg[i]; i++)
 	{
-		if (arg[A] == '-' && A == 0)
+		if (arg[i] == '-' && i == 0)
 			continue;
-		if (isdigit(arg[A]) == 0)
+		if (isdigit(arg[i]) == 0)
 			return (1);
 	}
 	return (0);
@@ -22,15 +23,15 @@ static int check_for_digit(char *arg)
 
 /**
  * m_push - push an integer onto the stack
- * @stack: double pointer to the begining of the stack
+ * @stack: double pointer to the beginning of the stack
  * @line_number: script line number
  * Return: void
  */
 
 void m_push(stack_t **stack, unsigned int line_number)
 {
-	char arg;
-	int i;
+	char *arg;
+	int n;
 
 	arg = strtok(NULL, "\n\t\r ");
 	if (arg == NULL || check_for_digit(arg))
@@ -40,9 +41,7 @@ void m_push(stack_t **stack, unsigned int line_number)
 			line_number);
 		exit(EXIT_FAILURE);
 	}
-
-	i = atoi(arg);
-
+	n = atoi(arg);
 	if (!add_node(stack, n))
 	{
 		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
